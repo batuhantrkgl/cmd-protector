853fd2dbdee1628ed4672edd9836f566f7b11912
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2022-02-19 11:04:43 +0300

    Add files via upload

--- cmdkoruma1/Form1.Designer.cs
+++ cmdkoruma1/Form1.Designer.cs
@@ -87,7 +87,7 @@
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cmd Protector | 1.0.1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
